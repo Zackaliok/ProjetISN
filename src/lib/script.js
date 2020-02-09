a1a1552dd6952f0bb016a76dd8707c446fdad3cf
SwenCake <60387414+SwnCake@users.noreply.github.com>
2020-02-09 13:28:04 +0100

    fzfzz

--- src/lib/script.js
+++ src/lib/script.js
@@ -10,3 +10,13 @@ function ajouterBloc(nom){
     AlgorithmeJoueur += "<p class='blocCommande'>"+nom+"</p>";
     zoneDuCode.innerHTML = AlgorithmeJoueur;
 }
+
+
+
+//========== NE PAS TOUCHER ==========
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.height = 600;
+canvas.width = 600;
+
