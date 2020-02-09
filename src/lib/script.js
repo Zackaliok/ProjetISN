f1929ebf08353c418ce7a5052e7657900465ab6b
SwenCake <60387414+SwnCake@users.noreply.github.com>
2020-02-09 14:37:27 +0100

    Ajout d'une fonction pour charger et afficher une image sur le canvas

--- src/lib/script.js
+++ src/lib/script.js
@@ -1,10 +1,19 @@
+//------------------------------ Initialisation ------------------------------
+
 alert("Yo la miff");
 
 var AlgorithmeJoueur = "";
 var CodeAlgorithmeJoueur = "";
-const zoneDuCode = document.getElementById("zoneDuCode");
-//Objet représentant l'element "zoneDuCode"
 
+const zoneDuCode = document.getElementById("zoneDuCode"); //Objet représentant l'element "zoneDuCode"
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+canvas.height = 600; canvas.width = 600;
+
+
+
+//------------------------------ Ajoute un bloc dans l'esapce de codage ------------------------------
 
 function ajouterBloc(nom){
     AlgorithmeJoueur += "<p class='blocCommande'>"+nom+"</p>";
@@ -12,8 +21,19 @@ function ajouterBloc(nom){
 }
 
 
-//==================== NE PAS SUPPRIMER ====================
+//------------------------------ Charger une image et l'afficher sur le canvas ------------------------------
+// La documentation arrive !
 
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-canvas.height = 600; canvas.width = 600;
+function loadImg(url){
+    return new Promise(resolve => {
+        const img = new Image();
+        img.addEventListener("load", () => {
+            resolve(img);
+        });
+        img.src = url;
+    });
+}
+
+loadImg("src/media/delete.png").then(img => {
+    ctx.drawImage(img,0,0); //TEST !!
+});
